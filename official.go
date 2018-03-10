42461fee09051550d40e51c81614df8c3194c730
Jeremy Rogers <rogersjn3@ornl.gov>
2018-03-10 16:13:57 -0500

    added player and official structs

---
+++ official.go
@@ -0,0 +1,16 @@
+package baseballSim
+
+import "time"
+
+// Official holds information about a referee or official
+type Official struct {
+	ID            string    `json:"id"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	FirstName     string    `json:"first_name"`
+	LastName      string    `json:"last_name"`
+	Name          string    `json:"name"`
+	Role          string    `json:"role"`
+	RoleLabel     string    `json:"role_label"`
+	UniformNumber int       `json:"uniform_number"`
+}
---
+++ player.go
@@ -0,0 +1,45 @@
+package baseballSim
+
+import "time"
+
+// Player holds information about a specific player
+type Player struct {
+	ID                   string    `json:"id"`
+	CreatedAt            time.Time `json:"created_at"`
+	UpdatedAt            time.Time `json:"updated_at"`
+	Active               bool      `json:"active"`
+	BirthDate            time.Time `json:"birth_date"`
+	Captain              string    `json:"captain"`
+	City                 string    `json:"city"`
+	Country              string    `json:"country"`
+	DraftSeason          string    `json:"draft_season"`
+	DraftRound           string    `json:"draft_round"`
+	DraftOverallPick     string    `json:"draft_overall_pick"`
+	DraftTeamName        string    `json:"draft_team_name"`
+	FirstName            string    `json:"first_name"`
+	Handedness           string    `json:"handedness"`
+	Bats                 string    `json:"bats"`
+	Height               float32   `json:"height"`
+	HighSchool           string    `json:"high_school"`
+	UnitOfHeight         string    `json:"unit_of_height"`
+	LastName             string    `json:"last_name"`
+	Name                 string    `json:"name"`
+	Nickname             string    `json:"nickname"`
+	PositionAbbreviation string    `json:"position_abbreviation"`
+	PositionName         string    `json:"reliever"`
+	Salary               int       `json:"salary"`
+	HumanizedSalary      string    `json:"humanized_salary"`
+	SalaryCurrency       string    `json:"salary_currency"`
+	School               string    `json:"school"`
+	Slug                 string    `json:"slug"`
+	Sport                string    `json:"sport"`
+	State                string    `json:"state"`
+	Weight               float32   `json:"weight"`
+	UniformNumber        int       `json:"uniform_number"`
+	UnitOfWeight         string    `json:"unit_of_weight"`
+	YearsOfExperience    int       `json:"years_of_experience"`
+	ProDebut             time.Time `json:"pro_debut"`
+	LeagueID             string    `json:"league_id"`
+	PlayingPositionID    string    `json:"playing_position_id"`
+	TeamID               string    `json:"team_id"`
+}
